ee8ce832e497726d036d0bba8b59b81caa94ad14
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-06 12:46:14 +1000

    Add support for disabled_extensions

--- modules/imagick/manifests/init.pp
+++ modules/imagick/manifests/init.pp
@@ -5,6 +5,12 @@ class imagick (
   $php = $config[php]
 ) {
 
+  if ( ! empty( $::config[disabled_extensions] ) and 'chassis/imagick' in $config[disabled_extensions] ) {
+    $package = absent
+  } else {
+    $package = latest
+  }
+
   if versioncmp( $php, '5.4') <= 0 {
     $php_package = 'php5'
   }
@@ -12,14 +18,14 @@ class imagick (
     $php_package = "php${php}"
   }
 
-  package { "${$php_package}-imagick":
-    ensure  => latest,
+  package { "php-imagick":
+    ensure  => $package,
     require => Package["${$php_package}-fpm"],
     notify  => Service["${$php_package}-fpm"]
   }
 
   package { 'imagemagick':
-    ensure  => latest,
+    ensure  => $package,
     require => Package["${$php_package}-fpm"],
     notify  => Service["${$php_package}-fpm"]
   }
